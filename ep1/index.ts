cbd4f9400dad9c6fea221d078b4912372d32a9db
jimmyjame <snaesedsdss@gmail.com>
2024-07-26 16:33:58 +0700

    การบ้าน

---
+++ ep1/index.ts
@@ -0,0 +1,19 @@
+const firstName : string = 'Akkasit_padungkij'
+
+let y : number = 10
+y = 20
+let ต้องการกลับบ้าน: boolean = true
+
+
+console.log(firstName)
+console.log(y)
+console.log(iWantToGoHome)
+
+
+
+const a : number = 6
+const b : number = 7
+
+console.log(a==b)
+console.log(a>b)
+console.log(a<=b)
