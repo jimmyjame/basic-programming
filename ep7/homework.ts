d1b5d8954bfe08124db0a61219f8bb555c264582
jimmyjame <snaesedsdss@gmail.com>
2024-09-13 14:23:19 +0700

    homework

---
+++ ep7/homework.ts
@@ -0,0 +1,15 @@
+const me = {
+    firstName: 'pari',
+    lastName: 'ki',
+    age: 18 ,
+    telephone: '112',
+    birthday: 29/12/2548,
+    address: 'bangu',
+    gender: 'male',
+    zipcode: '70077007',
+    road: 'hong',
+    email: 'dewdum@gmail.com',
+    district: 'preammy',
+    country: 'kakkook'
+    
+}
---
+++ ep7/index.ts
@@ -0,0 +1,100 @@
+// const score = [10, 23, 26, 30, 33]
+
+// let sum: number = 0
+
+// for (let i = 0; i < score.length; i++) {
+//     sum = sum + score [i] 
+// }
+
+// let avg = sum / score.length
+
+// console.log("Total: ",sum)
+// console.log("Avg: ", avg)
+
+// const myStudents: string = "เซน,แทน,เวฟ,รุ้ง,เท็ก,สร"
+
+// const myStudentsArray: string[] = myStudents.split(',')
+
+// console.log(myStudents)
+// console.log(myStudentsArray)
+
+// const myStudents: string = "เซน#แทน#เวฟ#รุ้ง#เท็ก#สร"
+
+// const myStudentsArray: string[] = myStudents.split('#')
+
+// console.log(myStudents)
+// console.log(myStudentsArray)
+
+// const fullName: string = 'สวยสุด สุดสวย'
+
+// console.log(fullName.split(' ')[0])
+
+// console.log(fullName.slice(0, fullName.indexOf(' ')))
+
+// const animallike: string = "แมว*หมา*ไดโนเสาร์*ฉลาม"
+
+// const animallikeArray: string[] = animallike.split('*')
+
+// console.log(animallike)
+// console.log(animallikeArray)
+
+// const animals: string = "แมว*หมา*ไดโนเสาร์*ฉลาม"
+// const animalsArray: string[] = animals.split('*')
+
+// console.log("ตั้งต้น: ", animalsArray)
+
+// animalsArray.pop() //เอาตัวสุดท้ายออก
+
+// console.log("เอาตัวสุดท้ายออก: ", animalsArray)
+
+// animalsArray.push('ยีราฟ') //ใส่ต่อท้าย
+
+// console.log("ใส่ต่อท้าย: ", animalsArray)
+
+// animalsArray.shift() //เอาตัวหน้าออก
+
+// console.log("เอาตัวสุดท้ายออก: ", animalsArray)
+
+// animalsArray.unshift('วูคอง') //ใส่ตัวหน้า
+
+// console.log("ใส่ตัวหน้า: ", animalsArray)
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"]
+
+// fruits.sort() //เรียง A - Z
+
+// console.log("เรียง A-Z: ", fruits)
+
+// fruits.reverse() //เรียง A - Z
+
+// console.log("เรียง Z-A: ", fruits)
+
+// fruits.slice(0,2)
+
+// console.log("ตัด: ", fruits)
+
+// const friendName: string = "คิว,รุจ,พีท,ปกป้อง"
+// const friendNameArray: string[] = friendName.split(',')
+
+// friendNameArray.sort()
+
+// console.log("เรียง ก - ฮ", friendNameArray)
+
+// friendNameArray.reverse()
+
+// console.log("เรียง ฮ - ก: ", friendNameArray)
+
+// const me = {
+//     firstname: 'Wasitpol',
+//     lastName: 'Silaruk',
+//     age: 18,
+//     birthday: 29/12/2005,
+//     telephone: 619619619,
+// }
+
+// ประวัติส่วนตัว
+// ชื่อ: Wasitpol
+// นามสกุล : Silaruk
+// อายุ: 18
+// วันเกิด:  29/12/2005
+//เบอร์โทร: 619619619
