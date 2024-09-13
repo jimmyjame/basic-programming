1f4cb4dcdee42f0acb65dd423f501deffceeec11
jimmyjame <snaesedsdss@gmail.com>
2024-09-13 14:27:21 +0700

    ตรรกะ

--- ep3/index.ts
+++ ep3/index.ts
@@ -1,57 +1,58 @@
-function helloworld() {
-    console.log("สวัสดี");    
+function helloWorld() {
+    console.log("สวัสดี")
 }
 
-helloworld()
+helloWorld() // ไม่มี input ไม่มี output
 
 function helloName(name: string) {
     console.log(name)
-}
+} 
 
-helloName('Jame')
+helloName('Ohm') // มี input ไม่มี output
 
 function getPi() {
     return 3.14
 }
 
-console.log(getPi())
+console.log(getPi()) // ไม่มี input มี output
 
 function st(fname: string, sname: string) {
     
-    if (fname === 'pari' && sname === 'jame') {
+    if (fname === 'mix' && sname === "zane") {
         console.log('เริ่มสอน')
-    } else {
+    }else {
         console.log('ไม่สอน')
     }
 }
 
-function st(fname: string, sname: string, aname: string) {
-    
-    if ((fname === 'mix' || sname === 'zane') && aname === 'forth') {
-        console.log('พร้อมเรียน')
-    } else {
+st('mix', 'zane')
+
+function sr(oname: string, hname: string, mname: string) {
+    if (oname === 'mix' || hname === 'zane' && mname === 'forth') {
+        console.log('พร้อมเรียน') 
+    }else {
         console.log('ยังไม่พร้อม')
     }
 }
 
-st('mix' ,'zane' ,'forth')
+sr('mix', 'zane', 'forth')
 
-function st(sex: string, high: number, weight: number) {
-    if (sex === 'ชาย' && high > 170 || weight > 50 && weight <= 110) {
+function qr(g: string, h: number, w: number) {
+    if(g === 'เพศชาย' && h > 170 || (w > 50 && w <= 110)) {
         console.log('จับใบดำใบแดง')
-    } else {
+    }else {
         console.log('ไม่เข้าเกณฑ์')
     }
 }
-st('ชาย' ,175 ,80)
 
-function st(age: number, เงินเดือน: number, เงินฝาก: number) {
-    if (age >= 16 && เงินเดือน < 70000 && เงินฝาก < 500000) {
-        console.log('รับ 10000บาท')
-    } else {
-        console.log('อด')
+qr('เพศชาย', 165, 45)
+
+function hk(age: number, pay: number, saving: number) {
+    if(age >= 16 && pay < 70000 && saving < 50000) {
+        console.log('รับ10000บาท')
+    }else {
+        console.log('อด.....')
     }
 }
-st(16 ,70000 ,500000)
 
-function st(high: number, )
+hk(18, 9000, 5600)
